157c5f4364feb58c80059c943dd5a854fa12f6e5
come_ill_foo <rayveleaveme34@inbox.ru>
2024-08-30 00:44:01 +0100

    test(ui): try publicPath to fix github-pages files handling

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-    configureWebpack:{
-      performance: {
-        hints: false
-      },
-      optimization: {
-        splitChunks: {
-          minSize: 10000,
-          maxSize: 250000,
-        }
+  publicPath: process.env.NODE_ENV === "production" ? "/linux_kernel_cves/" : "/",
+  configureWebpack:{
+    performance: {
+      hints: false
+    },
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000,
       }
     }
   }
+}
