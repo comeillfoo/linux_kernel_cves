485c620888714228503e4acf988404b7012a4ce5
come_ill_foo <rayveleaveme34@inbox.ru>
2024-08-31 23:48:08 +0100

    fix(ui): add trailing slash at router.base

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -6,7 +6,7 @@ import four from '@/components/four'
 Vue.use(Router)
 
 export default new Router({
-  base: '/linux_kernel_cves',
+  base: '/linux_kernel_cves/',
   mode: 'history',
   routes: [
     {
